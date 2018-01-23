47948c048996edf2641704a2593605d9b0d96fe0
Anders Olsen <anders.olsen@dof.dk>
2018-01-23 11:26:02 +0100

    Issue: #10 #9 #8 #7 #6 #5
    Removed the campaign part of the code so there's only 1 template
    
    Removed the partner fields and hardcoded DOFs contact details
    
    Removed the regards field and hardcoded it to 'Venlig hilsen / Best Regards'
    
    The direct phone number and skype id labels are hidden if the values are not set
    
    The partner phone number is locked to DOFs main number
    
    Forced a linebreak before the logos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent }   from './dashboard.component';
-import { CampaignComponent} from './campaign.component';
 
 const routes : Routes = [
-    {path : '', redirectTo: '/dashboard', pathMatch: 'full'},
-    {path : 'dashboard', component : DashboardComponent},    
-    {path : 'campaign', component : CampaignComponent }
+    {path : '', component : DashboardComponent},    
 ];
 
 @NgModule({
--- src/app/campaign.component.ts
+++ src/app/campaign.component.ts
@@ -15,78 +15,43 @@ export class CampaignComponent implements OnInit {
   htmlexport: string = "";
 
   c: Campaign = {
-    id: 1,
-    name: "Birdlife 2017",
-    regards_native: "XXX",
-    sender_name: "AAA",
-    sender_position_native: "YYY",
-    sender_position_english: "III",
-    sender_telephone: "#########",
-    sender_telephone_direct: "##########",
-    sender_skypeid: "aaa",
-    facebook_id: "bbb",
-    instagram_id: "ddd",
-    youtube_user: "uuu",
-    partner_logo: "/assets/small/DOF_80pct.jpg",
-    partner_name: "ppp",
-    partner_address: "adadad",
-    partner_zip: "zipzip",
-    partner_site_url: "url"
+    sender_name: "",
+    sender_position_native: "",
+    sender_position_english: "",
+    sender_telephone_direct: "",
+    sender_skypeid: "",
   };
 
 
   ngOnInit(): void {
     // this.getSomething()
     //   .then(with => this.campaign = with.somthing);
+    this.edit();
   }
 
   viewExample(): void {
-    this.showexport = false;
-    this.c= {
-    id: 1,
-    name: "Birdlife 2017",
-    regards_native: "Bedste hilsner",
+    this.showedit = true;
+    this.c = {
     sender_name: "Toke F. Nyborg",
     sender_position_native: "International medarbejder, naturpolitik & fundraising",
     sender_position_english: "International Officer, policy Advocacy & Fundraising",
-    sender_telephone: "+45 33283800",
     sender_telephone_direct: "+45 33283810",
     sender_skypeid: "tokenyborg",
-    facebook_id: "birdlifedk",
-    instagram_id: "danmarksfugle",
-    youtube_user: "DOFBirdlife",
-    partner_logo: "/assets/small/DOF_80pct.jpg",
-    partner_name: "DOF/Birdlife Denmark",
-    partner_address: "Vesterbrogade 140",
-    partner_zip: "1620 København V",
-    partner_site_url: "www.dof.dk"
   };
 }
 
   edit(): void {
     this.showedit = true;
     this.c = {
-      id: 1,
-      name: "Birdlife 2017 - Make your own",
-      regards_native: "",
       sender_name: "",
       sender_position_native: "",
       sender_position_english: "",
-      sender_telephone: "",
       sender_telephone_direct: "",
-      sender_skypeid: "",
-      facebook_id: "",
-      instagram_id: "",
-      youtube_user: "",
-      partner_logo: "",
-      partner_name: "",
-      partner_address: "",
-      partner_zip: "",
-      partner_site_url: ""
+      sender_skypeid: ""
     };
   };
   export(): void {
-    this.showexport = true;
+    this.showexport = !this.showexport;
     let styleElement : string = document.getElementsByTagName('style')[3].innerText;
     let htmlContent : string = document.getElementById('signature').outerHTML;
     this.htmlexport = `<html encoding="utf8"><body><style>` + styleElement + `</style>\n` + htmlContent + `</body></html>`;
