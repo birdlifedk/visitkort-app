fc95d05039e3964c6643070640206554c4da9162
Anders Olsen <anders.olsen@dof.dk>
2018-01-23 11:53:32 +0100

    Renamed the project to DOFs email signatur generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Birdlife\'s mail signature generator';
+  title = 'DOFs e-mail signatur generator';
 }
