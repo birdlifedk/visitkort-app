2b05f4048f8f5a3ea48ce8628cb7bdd10836f79e
Anders Olsen <anders.olsen@dof.dk>
2018-01-23 17:29:17 +0100

    Changed the result layout to be table based as Microsoft changed Outlook's rendering engine from WebKit to Word's Rendering Engine around Sept. 2016.
    Added a button to auto-copy the result

--- src/app/campaign.component.ts
+++ src/app/campaign.component.ts
@@ -12,6 +12,7 @@ export class CampaignComponent implements OnInit {
 
   showedit: boolean = false;
   showexport: boolean = false;
+  showcopyalert: boolean = false;
   htmlexport: string = "";
 
   c: Campaign = {
@@ -32,13 +33,13 @@ export class CampaignComponent implements OnInit {
   viewExample(): void {
     this.showedit = true;
     this.c = {
-    sender_name: "Toke F. Nyborg",
-    sender_position_native: "International medarbejder, naturpolitik & fundraising",
-    sender_position_english: "International Officer, policy Advocacy & Fundraising",
-    sender_telephone_direct: "+45 33283810",
-    sender_skypeid: "tokenyborg",
-  };
-}
+      sender_name: "Toke F. Nyborg",
+      sender_position_native: "International medarbejder, naturpolitik & fundraising",
+      sender_position_english: "International Officer, policy Advocacy & Fundraising",
+      sender_telephone_direct: "+45 33283810",
+      sender_skypeid: "tokenyborg",
+    };
+  }
 
   edit(): void {
     this.showedit = true;
@@ -50,10 +51,27 @@ export class CampaignComponent implements OnInit {
       sender_skypeid: ""
     };
   };
+
   export(): void {
     this.showexport = !this.showexport;
-    let styleElement : string = document.getElementsByTagName('style')[3].innerText;
-    let htmlContent : string = document.getElementById('signature').outerHTML;
+    let styleElement: string = document.getElementsByTagName('style')[3].innerText;
+    let htmlContent: string = document.getElementById('signature').outerHTML;
     this.htmlexport = `<html encoding="utf8"><body><style>` + styleElement + `</style>\n` + htmlContent + `</body></html>`;
   };
+
+  copy_text(element): void {
+    //Before we copy, we are going to select the text.
+    var text = document.getElementById('signature');
+    var selection = window.getSelection();
+    var range = document.createRange();
+    range.selectNodeContents(text);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    //add to clipboard.
+    document.execCommand('copy');
+    this.showcopyalert = true;
+    setTimeout(() => {
+      this.showcopyalert = false;
+    }, 3000);
+  }
 }
